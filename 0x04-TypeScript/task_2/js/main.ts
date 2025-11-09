6f6a61ec485a4932c5daa8fb989e1062d4c1ca54
ifeomasylviadike <ifeomasylvia.dike@gmail.com>
2025-11-09 06:20:17 +0100

    function

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,61 @@
+// DirectorInterface with 3 methods
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+// TeacherInterface with 3 methods
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+// Director class implementing DirectorInterface
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+// Teacher class implementing TeacherInterface
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+// createEmployee function that returns Director or Teacher based on salary
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+  return new Director();
+}
+
+// Test the function
+console.log(createEmployee(200));
+// Teacher
+
+console.log(createEmployee(1000));
+// Director
+
+console.log(createEmployee('$500'));
+// Director
